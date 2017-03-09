73fe157160926aa5eb5e9456f2d8672b4809deb3
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-93.local>
2017-03-09 13:38:12 +0100

    i just add the files for my src folder

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+(function(){
+var app = angular.module("invertedIndex", []);
+app.controller('IndexController', function(){
+	var books =  {
+    				"title": "Alice in Wonderland",
+    				"text": "Alice falls into a rabbit hole and enters a world full of imagination."
+  				}
+});
+
+})();
